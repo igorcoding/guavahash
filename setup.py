faa077448726f54f2c017bb9c0db7a0c2b297cbb
igorcoding <igorcoding@gmail.com>
2016-02-27 17:58:57 +0300

    MANIFEST file

--- setup.py
+++ setup.py
@@ -54,8 +54,23 @@ setup(name='guavahash',
       author_email='igorcoding@gmail.com',
       url='https://github.com/igorcoding/guavahash',
       download_url='https://github.com/igorcoding/guavahash/tarball/v' + version,
-      license='MIT',
+      license='MIT License',
       keywords='guava consistent hashing hash digest',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
       )
 
 if platform.system() == 'Windows':
